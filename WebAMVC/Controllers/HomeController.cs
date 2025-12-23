f445f7faadf0f4800358b7f49ac4d770dbbdcbad
Mohit verma <mohitverma98157899@example.com>
2025-12-23 19:29:27 +0530

    Add project files.

---
+++ WebAMVC/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebAMVC.Models;
+
+namespace WebAMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BlogDetails()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Projects()
+        {
+            return View();
+        }
+        public IActionResult ProjectDetails()
+        {
+            return View();
+        }
+        public IActionResult NotFound()
+        {
+            return View();
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebAMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
