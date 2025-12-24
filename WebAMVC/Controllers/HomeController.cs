3168ab591124d3b046be275406e02fe41bd77cec
Mohit verma <mohitverma98157899@example.com>
2025-12-24 20:04:11 +0530

    update

--- WebAMVC/Controllers/HomeController.cs
+++ WebAMVC/Controllers/HomeController.cs
@@ -15,35 +15,59 @@ namespace WebAMVC.Controllers
 
         public IActionResult Index()
         {
+            ViewData["Title"] = "Home";
+            ViewData["Description"] = "Home page description";
+            ViewData["Keywords"] = "Home, Home page";
             return View();
         }
 
         public IActionResult BlogDetails()
         {
+            ViewData["Title"] = "Blog-Details";
+            ViewData["Description"] = "Blog-Details page description";
+            ViewData["Keywords"] = "Blog, Blog details,blogs";
             return View();
         }
         public IActionResult About()
         {
+            ViewData["Title"] = "About";
+            ViewData["Description"] = "About page description";
+            ViewData["Keywords"] = "About, About page,about section";
             return View();
         }
         public IActionResult Blog()
         {
+            ViewData["Title"] = "Blog";
+            ViewData["Description"] = "Blog us page description";
+            ViewData["Keywords"] = "Blog, blog page";
             return View();
         }
         public IActionResult Contact()
         {
+            ViewData["Title"] = "Contact Us";
+            ViewData["Description"] = "Contact us page description";
+            ViewData["Keywords"] = "Contact, Contact page,Support";
             return View();
         }
         public IActionResult Projects()
         {
+            ViewData["Title"] = "Projects";
+            ViewData["Description"] = "Project page description";
+            ViewData["Keywords"] = "Project, Project page,Projects";
             return View();
         }
         public IActionResult ProjectDetails()
         {
+            ViewData["Title"] = "ProjectDetails";
+            ViewData["Description"] = "ProjectDetails page description";
+            ViewData["Keywords"] = "ProjectDetails, project detail,project";
             return View();
         }
         public IActionResult NotFound()
         {
+            ViewData["Title"] = "NotFound";
+            ViewData["Description"] = "Notfound page description";
+            ViewData["Keywords"] = "Error,Error page,Not found";
             return View();
         }
         
